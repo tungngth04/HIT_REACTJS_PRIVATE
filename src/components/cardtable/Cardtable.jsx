1118643c1c4b290b6e5dccdf1af390a635c65c1a
thanhtung <tungviet624@gmail.com>
2024-03-18 18:16:10 +0700

    add homework week 2

--- src/components/cardtable/Cardtable.jsx
+++ src/components/cardtable/Cardtable.jsx
@@ -1,27 +1,26 @@
+import "./cardtable.css";
 const Cardtable = ({ posts }) => {
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>title</td>
-                        <td>body</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {posts.map((post, index) => (
-                        <tr key={index}>
-                            <td>{post.id}</td>
-                            <td>{post.title}</td>
-                            <td>{post.body}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-
-    );
-}
-export default Cardtable
-
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>title</td>
+            <td>body</td>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post, index) => (
+            <tr key={index}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>{post.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default Cardtable;
